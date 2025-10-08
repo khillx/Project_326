fb036228f463d0fbbfd157bc63e89021e87c47d7
Nicholas Marchese <150188487+khillx@users.noreply.github.com>
2025-10-07 22:31:12 -0500

    Add AuthService class with methods for account management

---
+++ services/auth_service.py
@@ -0,0 +1,15 @@
+class AuthService:
+    def create_account(self, email, pwd, gamer_tag):
+        pass
+
+    def login(self, email, pwd):
+        pass
+
+    def logout(self, user_id):
+        pass
+
+    def reset_password(self, email):
+        pass
+
+    def verify_account(self, token):
+        pass
