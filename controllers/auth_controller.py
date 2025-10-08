aad7e156f0f611d33018f01d687c1d99d2abeb2f
Nicholas Marchese <150188487+khillx@users.noreply.github.com>
2025-10-07 22:31:20 -0500

    Add AuthController class with sign up, sign in, sign out, and reset password methods

---
+++ controllers/auth_controller.py
@@ -0,0 +1,12 @@
+class AuthController:
+    def sign_up(self, data):
+        pass
+
+    def sign_in(self, data):
+        pass
+
+    def sign_out(self):
+        pass
+
+    def reset_password(self, email):
+        pass
