5e11efb91d1898af99e1b8960b5557fbaa1bb1f3
johnknappiii <johnknappiii@gmail.com>
2025-10-31 16:18:36 -0500

    Update stars_rating.js

--- repositories/stars_rating.js
+++ repositories/stars_rating.js
@@ -1,34 +1,26 @@
+// static/js/stars_ratings.js
 let stars = document.getElementsByClassName("star");
-let output = document.getElementById("output");
+let currentRating = 0;
 
 function starClick(numStars) {
+    currentRating = numStars;
     let j = 0;
     while (j < 5) {
         stars[j].className = "star";
         j++;
     }
 
-    for (let i=0; i<numStars; i++) {
+    for (let i = 0; i < numStars; i++) {
+        let ratingClass = "";
         switch (numStars) {
-            case 1:
-                stringNum = "one";
-                break;
-            case 2:
-                stringNum = "two";
-                break;
-            case 3:
-                stringNum = "three";
-                break;
-            case 4:
-                stringNum = "four";
-                break;
-            case 5:
-                stringNum = "five";
-                break;
-            default:
-                stringNum = "";
-                break;
+            case 1: ratingClass = "one"; break;
+            case 2: ratingClass = "two"; break;
+            case 3: ratingClass = "three"; break;
+            case 4: ratingClass = "four"; break;
+            case 5: ratingClass = "five"; break;
         }
-        stars[i].className = "star " + stringNum;
+        stars[i].className = "star " + ratingClass;
     }
 }
+
+window.getCurrentRating = () => currentRating;
