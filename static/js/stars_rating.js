00ee1475765e26def55f63abd5700443372240cb
johnknappiii <johnknappiii@gmail.com>
2025-11-02 18:42:13 -0600

    Updated File Structure - Overall QoL fix
    We are going to keep the file structure like this throughout the entire repository. This is the only the way to run main.py. It is stupid to have to change where every file is located to run and test our code.

---
+++ static/js/stars_rating.js
@@ -0,0 +1,26 @@
+// static/js/stars_ratings.js
+let stars = document.getElementsByClassName("star");
+let currentRating = 0;
+
+function starClick(numStars) {
+    currentRating = numStars;
+    let j = 0;
+    while (j < 5) {
+        stars[j].className = "star";
+        j++;
+    }
+
+    for (let i = 0; i < numStars; i++) {
+        let ratingClass = "";
+        switch (numStars) {
+            case 1: ratingClass = "one"; break;
+            case 2: ratingClass = "two"; break;
+            case 3: ratingClass = "three"; break;
+            case 4: ratingClass = "four"; break;
+            case 5: ratingClass = "five"; break;
+        }
+        stars[i].className = "star " + ratingClass;
+    }
+}
+
+window.getCurrentRating = () => currentRating;
